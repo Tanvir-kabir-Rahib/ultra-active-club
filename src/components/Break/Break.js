c53d80714488e410ad115e7f75b942e41793ca39
Tanvir-kabir-Rahib <tanvirkabirrahib2@gmail.com>
2022-09-29 17:40:45 +0600

    second commit

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -3,16 +3,16 @@ import "./Break.css"
 
 const Break = () => {
     const breakHandler = () => {
-
+        
     }
     return (
-        <div className='breaks bg-secondary rounded'>
-            <p className='bg-white p-2 rounded-circle'>10s</p>
-            <p className='bg-white p-2 rounded-circle'>20s</p>
-            <p className='bg-white p-2 rounded-circle'>30s</p>
-            <p className='bg-white p-2 rounded-circle'>40s</p>
-            <p className='bg-white p-2 rounded-circle'>50s</p>
-            <p className='bg-white p-2 rounded-circle'>60s</p>
+        <div className='breaks d-flex justify-content-around align-items-center py-2 px-1 me-1 rounded'>
+            <p className='mb-0 bg-white p-2 rounded-circle'>10s</p>
+            <p className='mb-0 bg-white p-2 rounded-circle'>20s</p>
+            <p className='mb-0 bg-white p-2 rounded-circle'>30s</p>
+            <p className='mb-0 bg-white p-2 rounded-circle'>40s</p>
+            <p className='mb-0 bg-white p-2 rounded-circle'>50s</p>
+            <p className='mb-0 bg-white p-2 rounded-circle'>60s</p>
         </div>
     );
 };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,16 +6,30 @@ import Break from '../Break/Break';
 const Profile = () => {
     return (
         <div>
-            <div className='person-data'>
+            <div className='d-flex align-items-center mb-5'>
                 <img className='profile' src={profile} alt="" />
                 <div>
-                    <h2 className='person-name'> Tanvir Kabir</h2>
-                    <p className='location'>Khulna, Bangladesh</p>
+                    <h4 className='mb-0'> Tanvir Kabir</h4>
+                    <p className='mb-0 text-secondary'>Khulna, Bangladesh</p>
                 </div>
             </div>
-            <h3>Add A Break</h3>
+            <div className='d-flex align-items-center justify-content-evenly back-ground py-3 rounded mb-5'>
+                <div className='bg-light rounded-circle detail'>
+                    <p className='mb-0 text-center fw-semibold'>75<sub>kg</sub></p>
+                    <p className='mb-0 text-center fw-semibold'>Weight</p>
+                </div>
+                <div className='bg-light rounded-circle detail'>
+                    <p className='mb-0 text-center fw-semibold'>5.7<sub>Feet</sub></p>
+                    <p className='mb-0 text-center fw-semibold'>Height</p>
+                </div>
+                <div className=" bg-light rounded-circle detail">
+                    <p className='mb-0 text-center fw-semibold'>17<sub>yrs</sub></p>
+                    <p className='mb-0 text-center fw-semibold'>Age</p>
+                </div>
+            </div>
+            <h3 className='mb-3'>Add A Break</h3>
             <Break></Break>
-            <div>
+            <div className='mt-5'>
                 <h3>Exercise Details</h3>
                 <div>
 
@@ -24,7 +38,7 @@ const Profile = () => {
 
                 </div>
             </div>
-            <button className="btn btn-lg btn-primary">
+            <button className="btn btn-lg btn-primary mt-5">
                 Activity Completed
             </button>
 
