bf13589e154d8ced9fc0d548a3df3d5ce6e031b8
Tanvir-kabir-Rahib <tanvirkabirrahib2@gmail.com>
2022-09-29 19:03:20 +0600

    fakeData added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
+import Exercises from './components/Exercises/Exercises';
 
 function App() {
   return (
     <div className="App page-griding">
       <div className=''>
         <Header></Header>
+        <Exercises></Exercises>
       </div>
       <div>
         <Profile></Profile>
--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Exercises = () => {
+    const [exercises, setExercies] = useState([])
+    useEffect(()=>{
+        fetch("../../fakeData/exercises.JSON")
+        .then(res => res.json())
+        .then(data => {
+            setExercies(data)
+        })
+    },[])
     return (
-        <div>
-            
+        <div className='bg-light rounded-5'>
+            <h3>Select Today's Exercise</h3>
+            <div>
+
+            </div>
         </div>
     );
 };
